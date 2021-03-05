953e238de36b03184b25debae90becc6ed24ebd6
jchow <jchow@ensignservices.net>
2021-03-04 18:16:53 -0800

    create corporation class

--- models/MoneyModel.py
+++ models/MoneyModel.py
@@ -2,7 +2,7 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 
 
-class MoneyAgent(Agent):
+class Person(Agent):
     """An agent with fixed initial wealth."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
@@ -22,6 +22,27 @@ class MoneyAgent(Agent):
               )
 
 
+class Corporation(Agent):
+    """An agent with fixed initial wealth."""
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.wealth = 100
+
+    def step(self):
+        print(f'Corporation number {self.unique_id} has {self.wealth} dollars')
+        if self.wealth == 0:
+            return
+        # todo what do corporations do at steps?
+        # other_agent = self.random.choice(self.model.schedule.agents)
+        # other_agent.wealth += 1
+        # self.wealth -= 1
+        # print(f"""
+        #         Agent {self.unique_id} gave Agent {other_agent.unique_id} 1 dollar.
+        #         Agent {self.unique_id} has {self.wealth} dollars.
+        #         """
+        #       )
+
+
 class MoneyModel(Model):
     """A model with some number of agents."""
     def __init__(self, num_agents, num_corps):
@@ -29,7 +50,7 @@ class MoneyModel(Model):
         self.schedule = RandomActivation(self)
         # Create agents
         for i in range(self.num_agents):
-            a = MoneyAgent(i, self)
+            a = Person(i, self)
             self.schedule.add(a)
 
     def step(self):
