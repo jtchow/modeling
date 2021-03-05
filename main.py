9fd9a3bcee6ca6bb8e5d15a2afa3607ab512723e
jchow <jchow@ensignservices.net>
2021-03-04 18:28:00 -0800

    create basic model that has people giving money to corporations

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    all_wealth = []
-    for run in range(100):
-        model = MoneyModel(10)
-        for i in range(10):
-            model.step()
-        for agent in model.schedule.agents:
-            all_wealth.append(agent.wealth)
-    plt.hist(all_wealth, bins=range(max(all_wealth) + 1))
+    model = MoneyModel(10, 3)
+    for i in range(10):
+        model.step()
+    for corp in model.corp_schedule.agents:
+        print(corp.wealth)
+    # all_wealth = []
+    # for run in range(100):
+    #     model = MoneyModel(10)
+    #     for i in range(10):
+    #         model.step()
+    #     for agent in model.schedule.agents:
+    #         all_wealth.append(agent.wealth)
+    # plt.hist(all_wealth, bins=range(max(all_wealth) + 1))
--- models/MoneyModel.py
+++ models/MoneyModel.py
@@ -6,17 +6,17 @@ class Person(Agent):
     """An agent with fixed initial wealth."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.wealth = 1
+        self.wealth = 5
 
     def step(self):
         print(f'I am agent number {self.unique_id} and I have {self.wealth} dollars')
         if self.wealth == 0:
             return
-        other_agent = self.random.choice(self.model.schedule.agents)
-        other_agent.wealth += 1
+        selected_corp = self.random.choice(self.model.corp_schedule.agents)
+        selected_corp.wealth += 1
         self.wealth -= 1
         print(f"""
-                Agent {self.unique_id} gave Agent {other_agent.unique_id} 1 dollar. 
+                Agent {self.unique_id} gave Corporation {selected_corp.unique_id} 1 dollar. 
                 Agent {self.unique_id} has {self.wealth} dollars.
                 """
               )
@@ -32,6 +32,7 @@ class Corporation(Agent):
         print(f'Corporation number {self.unique_id} has {self.wealth} dollars')
         if self.wealth == 0:
             return
+        print(f'Corporation number {self.unique_id} does something')
         # todo what do corporations do at steps?
         # other_agent = self.random.choice(self.model.schedule.agents)
         # other_agent.wealth += 1
@@ -46,12 +47,19 @@ class Corporation(Agent):
 class MoneyModel(Model):
     """A model with some number of agents."""
     def __init__(self, num_agents, num_corps):
-        self.num_agents = num_agents, num_corps
-        self.schedule = RandomActivation(self)
+        self.num_agents = num_agents
+        self.num_corps = num_corps
+        self.agent_schedule = RandomActivation(self)
+        self.corp_schedule = RandomActivation(self)
         # Create agents
         for i in range(self.num_agents):
             a = Person(i, self)
-            self.schedule.add(a)
+            self.agent_schedule.add(a)
+
+        for i in range(self.num_corps):
+            corp = Corporation(i, self)
+            self.corp_schedule.add(corp)
 
     def step(self):
-        self.schedule.step()
+        self.agent_schedule.step()
+        self.corp_schedule.step()
